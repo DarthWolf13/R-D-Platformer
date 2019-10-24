c47d0a5fcf9f0b57180502af6d7532575d1b00a6
Martijn van der Meer <martijn.vander.meer@quicknet.nl>
2019-10-24 15:09:35 +0200

    Horizontal Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
