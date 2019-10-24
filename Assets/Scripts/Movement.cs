bf5a2f2f4e647145b4689deb0ab7b075e9d3f676
Martijn van der Meer <martijn.vander.meer@quicknet.nl>
2019-10-24 15:42:04 +0200

    Jumping

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpSpeed = 5f;
+    public bool Grounded = false;
     void Start()
     {
         
@@ -12,7 +14,16 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+        Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && Grounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpSpeed), ForceMode2D.Impulse);
+        }
     }
 }
